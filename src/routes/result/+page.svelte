dd2c9ec9dc6dbb69710f655564d0599d86268260
mokmokahr <jbmok.workspace@gmail.com>
2023-05-15 08:39:38 +0900

    ADD: Add result case

--- src/routes/result/+page.svelte
+++ src/routes/result/+page.svelte
@@ -1,6 +1,76 @@
 <script>
     import {data} from '../../store';
+    /**
+   * @type {string}
+   */
+    let result,charDesc;
     console.log("asdf");
     console.log($data);
+    switch($data){
+        case 'istj':
+            result = "기사";
+            charDesc = "";
+            break;
+        case 'estj':
+            result = "";
+            charDesc = "";
+            break;
+        case 'intj':
+            result = "";
+            charDesc = "";
+            break;
+        case 'entj':
+            result = "";
+            charDesc = "";
+            break;
+        case 'infj':
+            result = "";
+            charDesc = "";
+            break;
+        case 'enfj':
+            result = "";
+            charDesc = "";
+            break;
+        case 'isfj':
+            result = "";
+            charDesc = "";
+            break;
+        case 'esfj':
+            result = "";
+            charDesc = "";
+            break;
+        case 'infp':
+            result = "";
+            charDesc = "";
+            break;
+        case 'enfp':
+            result = "";
+            charDesc = "";
+            break;
+        case 'isfp':
+            result = "";
+            charDesc = "";
+            break;
+        case 'esfp':
+            result = "";
+            charDesc = "";
+            break;
+        case 'intp':
+            result = "";
+            charDesc = "";
+            break;
+        case 'entp':
+            result = "";
+            charDesc = "";
+            break;
+        case 'istp':
+            result = "";
+            charDesc = "";
+            break;
+        case 'estp':
+            result = "";
+            charDesc = "";
+            break;
+    }
 </script>
-<p>{$data}</p> 
+<p>{result}</p> 
